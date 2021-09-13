caae7cddf3e4f80a8fad38aea06e22604ea08a7e
Prikshit-anthal <prikshit_12013102@nitkkr.ac.in>
2021-09-13 12:50:47 +0530

    added upload feature in user section used roll no as id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')//path module import
 var exphbs = require('express-handlebars')
 const app = express()
-const port = 3005
+const port = 5010
 
 
 app.engine('handlebars',exphbs());
